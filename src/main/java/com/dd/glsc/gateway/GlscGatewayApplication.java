113179a8d963ef73794c2ab58abfb6ea80ffd46e
Administrator <18211882344@163.com>
2025-12-23 17:04:50 +0800

    初始化

---
+++ src/main/java/com/dd/glsc/gateway/GlscGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.dd.glsc.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GlscGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlscGatewayApplication.class, args);
+    }
+
+}
